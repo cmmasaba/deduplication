9461ab9caba41dbbffa4c0957765224bb5a9b784
cmmasaba <mmasabacollins9@gmail.com>
2025-12-18 22:50:49 +0300

    example

--- example.go
+++ example.go
@@ -7,17 +7,24 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
-	var dedup Deduplicator
-	dedup.applyDefaults()
-	vals := []string{"Nairobi", "Mombasa", "Kisumu", "Nakuru"}
+	var (
+		dedup     Deduplicator
+		readLimit int64 = 64
+	)
 
+	dedup.applyDefaults()
+	dedup.KeyFactory = NewValueHasherSHA256(int64(readLimit))
+
+	ctx := context.Background()
 	timeoutCtx, _ := context.WithTimeout(ctx, 5*time.Minute)
+	vals := []string{"Nairobi", "Mombasa", "Kisumu", "Nakuru"}
 	ticker := time.NewTicker(20 * time.Second)
+
 outer:
 	for {
 		select {
 		case <-timeoutCtx.Done():
+			fmt.Println("\n\nDone")
 			break outer
 		case <-ticker.C:
 			for i := range 4 {
