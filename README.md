2caa82f5ef044c558c0faa44b75dbbc916e237cc
Collins Mmasaba <mmasabacollins9@gmail.com>
2025-12-18 22:52:43 +0300

    final implementation and example (#3)
    * chore: documentation
    
    * feat: added redis
    
    * example
    
    * example

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Expired Key Repository
 
-An expired key repository is basically a key-value store where the keys have time-to-live and expire once the<br>
-time elapses, after which it should be automatically removed from the repository along with its value.<br><br>
+An expired key repository is a store for key-value pairs. The key-value pairs are only valid for a specified time duration, after which they are automatically cleared out<br>
 
 Example usecases:
 
@@ -11,23 +10,9 @@ Example usecases:
 - store for auth tokens, OTP codes and password reset codes that should be valid for a preset duration
 - deduplication in an asynchronous message-oriented middleware using at-least-once delivery
 
-This implementation was inspired by the last use case above. I was implementing pub/sub messaging for a<br>project
-at work using [Watermill](https://watermill.io/docs/getting-started/). One of the configurations you can specify for
-message delivery guarantees is<br>
-`at-least-once delivery`. The publisher will try sending the message until it gets an acknowledgement from<br>
-the subscriber.<br>
-This is good for reliability but there's always likelihood that the message will be delivered more than once<br>
-to the subscriber. If duplicates are not a big deal in your application then that's no problem, but in most<br>
-cases you want the application to be idempotent and this where things get interesting. The approach used by<br>
-Watermill for deduplication is by using a key-value store with expiring keys. The underlying key-value store<br>
-used is a hash map with a mutex for concurrency control. According to the Watermill docs, the downside of<br>
-this implementation is that:
-<i>
+This implementation was inspired by the last use case above. For example in pub/sub, the publisher sends messages until the subscriber acknowleges them. To maintain idempotency, the deduplicator drops messages it marks as duplicates within a specified time window.
+In my implementation the repository is backed by Redis which has good benefits:
 
-```
-The state **cannot be shared or synchronized between instances** by design for performance.
-```
-
-</i>
-In this implementation I will try building a KV repository with expiring keys backed by an in-memory KV<br>
-store, specifically Redis but you can swap it out for your preferred choice
+- highly efficient for lookup operations
+- automatic cleanup of keys afte expiry
+- supports distributed workloads
---
+++ expiring_key_repo.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/gob"
+	"errors"
+	"hash/adler32"
+	"io"
+	"math"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/extra/redisotel/v9"
+	"github.com/redis/go-redis/v9"
+)
+
+// ValueHasherLimitMinimum is the least number of bytes used for
+// calculating the hash value of a key
+const ValueHasherLimitMinimum = 64
+
+// Payload is a value to be deduplicated.
+// Key is a hash of the value
+// Value is the actual data
+type Payload struct {
+	Key   string
+	Value any
+}
+
+type Cache interface {
+	Exists(context.Context, ...string) *redis.IntCmd
+	SetEx(context.Context, string, any, time.Duration) *redis.StatusCmd
+	DBSize(context.Context) *redis.IntCmd
+}
+
+type ExpiringKeyRepository interface {
+	// IsDuplicate returns `true` if the key
+	// was not checked in recent past.
+	// The key must expire in a certain time window.
+	IsDuplicate(ctx context.Context, data Payload) (ok bool, err error)
+}
+
+// ValueHasher returns a hash that identifies a value.
+// The hash should be unique per value but avoiding collisions
+// completely is not practical.
+type ValueHasher func(value []byte) (string, error)
+
+// Deduplicator drops similar values if they are present in a
+// [ExpiringKeyRepository]. The similarity is determined by
+// [ValueHasher]. Timeout is applied using [context.WithTimeout]
+//
+// KeyFactory defaults to [NewValueHasherAdler32] with read limit
+// set to [math.MaxInt64]. Use [NewValueHasherSHA256] for less
+// collisions.
+// Timeout defaults to one minute.
+type Deduplicator struct {
+	KeyFactory ValueHasher
+	Repository ExpiringKeyRepository
+	Timeout    time.Duration
+}
+
+// IsDuplicate returns `true` if the value hash calculated by [ValueHasher]
+// was seen in a deduplication time window.
+func (d *Deduplicator) IsDuplicate(ctx context.Context, value any) (bool, error) {
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(value)
+	if err != nil {
+		return false, err
+	}
+
+	key, err := d.KeyFactory(buf.Bytes())
+	if err != nil {
+		return false, err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, d.Timeout)
+	defer cancel()
+
+	return d.Repository.IsDuplicate(ctx, Payload{Key: key, Value: value})
+}
+
+func (d *Deduplicator) applyDefaults() {
+	if d == nil {
+		r, err := NewRedisExpiringKeyRepo(time.Minute, os.Getenv("REDIS_HOST_URL"))
+		if err != nil {
+			panic(err)
+		}
+
+		d = &Deduplicator{
+			KeyFactory: NewValueHasherAdler32(math.MaxInt64),
+			Repository: r,
+			Timeout:    5*time.Minute,
+		}
+	}
+	if d.KeyFactory == nil {
+		d.KeyFactory = NewValueHasherAdler32(math.MaxInt64)
+	}
+
+	if d.Repository == nil {
+		r, err := NewRedisExpiringKeyRepo(5*time.Minute, os.Getenv("REDIS_HOST_URL"))
+		if err != nil {
+			panic(err)
+		}
+		d.Repository = r
+	}
+
+	if d.Timeout < 5*time.Millisecond {
+		d.Timeout = 5 * time.Millisecond
+	}
+}
+
+func NewCache(connStr string) (Cache, error) {
+	opt, err := redis.ParseURL(connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	client := redis.NewClient(opt)
+
+	// Enable tracing instrumentation.
+	if err := redisotel.InstrumentTracing(client); err != nil {
+		return nil, err
+	}
+
+	// Enable metrics instrumentation.
+	if err := redisotel.InstrumentMetrics(client); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+type RedisExpiringKeyRepo struct {
+	cache  Cache
+	window time.Duration
+}
+
+func NewRedisExpiringKeyRepo(window time.Duration, redisURL string) (*RedisExpiringKeyRepo, error) {
+	if window < time.Millisecond*5 {
+		return nil, errors.New("deduplication window cannot be less than 5 milliseconds")
+	}
+
+	c, err := NewCache(redisURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RedisExpiringKeyRepo{
+		cache:  c,
+		window: window,
+	}, nil
+}
+
+// IsDuplicate checks if the `data` is duplicate within a given time window.
+func (r *RedisExpiringKeyRepo) IsDuplicate(ctx context.Context, data Payload) (bool, error) {
+	exists, err := r.cache.Exists(ctx, data.Key).Result()
+	if err != nil {
+		return false, err
+	}
+
+	if exists > 0 {
+		return true, nil
+	}
+
+	err = r.cache.SetEx(ctx, data.Key, data.Value, r.window).Err()
+	if err != nil {
+		return false, err
+	}
+
+	return false, nil
+}
+
+// Len returns the number of keys in the repository that have not expired.
+func (r *RedisExpiringKeyRepo) Len(ctx context.Context) (int64, error) {
+	count, err := r.cache.DBSize(ctx).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+// NewValueHasherAdler32 generates messages using Adler-32 checksum of the [Value].
+// Read limit specifies how many bytes of the [Value] are used to compute the hash.
+// Lower limits improve performance but results in more false positives. Read limit
+// must be greater than [HasherLimitMinimum]
+func NewValueHasherAdler32(readLimit int64) ValueHasher {
+	if readLimit < ValueHasherLimitMinimum {
+		readLimit = ValueHasherLimitMinimum
+	}
+
+	return func(value []byte) (string, error) {
+		h := adler32.New()
+		_, err := io.CopyN(h, bytes.NewReader(value), readLimit)
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+
+		return string(h.Sum(nil)), nil
+	}
+}
+
+// NewValueHasherSHA256 is slower but more efficient. Read limit specifies how many
+// bytes of the [Value] are used to compute the hash.
+// Lower limits improve performance but results in more false positives. Read limit
+// must be greater than [HasherLimitMinimum]
+func NewValueHasherSHA256(readLimit int64) ValueHasher {
+	if readLimit < ValueHasherLimitMinimum {
+		readLimit = ValueHasherLimitMinimum
+	}
+
+	return func(value []byte) (string, error) {
+		h := sha256.New()
+		_, err := io.CopyN(h, bytes.NewReader(value), readLimit)
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+
+		return string(h.Sum(nil)), nil
+	}
+}
